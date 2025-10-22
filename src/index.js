5efd3552833f4db08bf1b3bcaa1fb135dcce98dc
korney913 <korney123123@gmail.com>
2025-10-22 18:20:02 +0300

    Добавлен сервер с FCM через Firebase Admin SDK

--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ app.use(express.json());
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
 });
 
 // POST /send-notification
